837422c23892f0f49c597dcacba558267ea40817
Saral Bajracharya <69418398+saralbjr@users.noreply.github.com>
2026-05-21 16:32:54 +0545

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = "https://saralbajracharya.com.np";
+  const baseUrl = "https://saralbjr.com.np";
 
   return {
     rules: [
