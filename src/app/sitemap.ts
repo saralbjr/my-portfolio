95ad994b6dc430dd58d6a487a348732b21a893f6
Saral Bajracharya <69418398+saralbjr@users.noreply.github.com>
2026-05-21 16:33:15 +0545

    Update base URL in sitemap function

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://saralbajracharya.com.np";
+  const baseUrl = "https://saralbjr.com.np";
 
   return [
     {
